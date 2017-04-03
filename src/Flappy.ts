659fd6ba37d11b8bb8cb26920417a1d6d487bdc2
Ugho Stephan <ugho.stephan@consertotech.pro>
2017-04-03 17:42:31 +0200

    minor changes

--- src/Flappy.ts
+++ src/Flappy.ts
@@ -8,62 +8,57 @@ export class Flappy {
     bird: Phaser.Sprite;
     pipes: Phaser.Sprite[] = [];
 
+    constructor() {
+        this.game = new Phaser.Game(this.gameWidth, this.gameHeight, Phaser.AUTO, "content", this.mainState);
+        /*this.game.state.add("main", this.mainState);
+        this.game.state.start("main");*/
+    }
+
     mainState = {
-        preload: this.preload,
-        create: this.create,
-        update: this.update,
-        jump: this.jump,
-        restartGame: this.restartGame
+        preload: () => {
+            this.game.load.image('background', '/assets/background.jpg');
+            this.game.load.image('bird', '/assets/bird.png');
+            this.game.load.image('pipe', '/assets/pipe.png');
+            this.game.load.audio('jump', '/assets/jump.wav');
+        },
+
+        create: () => {
+            // Set the background
+            this.game.add.sprite(0, 0, 'background');
+            // Set the physics system
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            // Display the bird at the position x=100 and y=245
+            this.bird = this.game.add.sprite(100, 245, 'bird');
+            // Add physics to the bird
+            // Needed for: movements, gravity, collisions, etc.
+            this.game.physics.arcade.enable(this.bird);
+            // Add gravity to the bird to make it fall
+            this.bird.body.gravity.add(0, 1000);
+            // Call the 'jump' function when the spacekey is hit
+            let spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+            spaceKey.onDown.add(this.jump);
+        },
+
+        update: () => {
+            // If the bird is out of the screen (too high or too low)
+            // Call the 'restartGame' function
+
+            if (this.bird.y < 0 || this.bird.y > this.gameHeight) {
+                this.restartGame();
+            }
+        },
+
+        jump: () => {
+            console.log("jump called");
+            // Add a vertical velocity to the bird
+            this.bird.body.velocity.add(0, -250);
+        },
+
+        restartGame: () => {
+            console.log("you loose");
+            //this.
+            this.game.state.restart();
+        }
     };
 
-    constructor() {
-        this.game = new Phaser.Game(this.gameWidth, this.gameHeight, Phaser.AUTO, "content");
-        this.game.state.add("main", this.mainState);
-        this.game.state.start("main");
-    }
-
-    preload() {
-        this.game.load.image('background', '/assets/background.jpg');
-        this.game.load.image('bird', '/assets/bird.png');
-        this.game.load.image('pipe', '/assets/pipe.png');
-        this.game.load.audio('jump', '/assets/jump.wav');
-    }
-
-    create() {
-        // Set the background
-        this.game.add.sprite(0, 0, 'background');
-        // Set the physics system
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        // Display the bird at the position x=100 and y=245
-        this.bird = this.game.add.sprite(100, 245, 'bird');
-        // Add physics to the bird
-        // Needed for: movements, gravity, collisions, etc.
-        this.game.physics.arcade.enable(this.bird);
-        // Add gravity to the bird to make it fall
-        this.bird.body.gravity.add(0, 1000);
-        // Call the 'jump' function when the spacekey is hit
-        let spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        spaceKey.onDown.add(this.jump, this);
-    }
-
-    update() {
-        // If the bird is out of the screen (too high or too low)
-        // Call the 'restartGame' function
-
-        if (this.bird.y < 0 || this.bird.y > this.gameHeight) {
-            this.restartGame();
-        }
-    }
-
-    jump() {
-        console.log("jump called");
-        // Add a vertical velocity to the bird
-        this.bird.body.velocity.add(0, -250);
-    }
-
-    restartGame() {
-        console.log("you loose");
-        //this.
-        this.game.state.restart();
-    }
 }
